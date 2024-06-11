2675def17f85534dc108286cd53917f3e2ac4fe3
이연승 <leeys951006@gmail.com>
2024-06-11 11:01:06 +0900

    1

---
+++ work-2.js
@@ -0,0 +1,18 @@
+function formatHTML() {
+  let htmlData = {
+    title: '이연승',
+    h1: '내이름은',
+    p: '이연승입니다',
+  };
+  let html = `
+  <html>
+  <head>
+  <title></title>
+  </head>
+  <body>
+  <h1></h1>
+  <p></p>
+  </body>
+  </html>
+  `;
+}
